73d154ab705398fe8054a78a388ee43f200517f7
surender singh <singhsurender28231@gmail.com>
2025-02-27 00:38:13 +0530

    fixed video bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import Navbar from '../components/NavBar'
+import React, { useState } from 'react';
+import Navbar from '../components/NavBar';
+import Loading from './LoadingPage'
 
 const Home = () => {
-    // rgb(250,148,39)
-  return (
-    <div id='home' className="fixed inset-0 z-0">
-      <Navbar />
-    <video
-      autoPlay
-      loop
-      muted
-      playsInline
-      className="object-cover w-full h-full"
-    >
-      <source src="./videos/goku.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-    {/* Translucent overlay on video */}
-    <div className="absolute inset-0 bg-black/55"></div>
-  </div>
-  )
-}
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
-export default Home
+  return (
+    <div id="home" className="fixed inset-0 z-0">
+      <Navbar />
+      {/* Loading overlay */}
+      {!videoLoaded && (
+        <Loading />
+      )}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        onLoadedData={() => setVideoLoaded(true)}
+        className={`object-cover w-full h-full transition-opacity duration-500 ${
+          videoLoaded ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        <source src="./videos/goku.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      {/* Translucent overlay */}
+      <div
+        className={`absolute inset-0 bg-black/55 transition-opacity duration-500 ${
+          videoLoaded ? 'opacity-100' : 'opacity-0'
+        }`}
+      ></div>
+    </div>
+  );
+};
+
+export default Home;
